20d0999ca0e01585e541dcc6a63eef94d8c55218
Guigsz <guilhermeschmitzguii@gmail.com>
2023-01-26 18:40:55 -0300

    update

---
+++ script.js
@@ -0,0 +1,39 @@
+async function getServerData(urls) {
+    try {
+        const responses = await Promise.all(urls.map(url => fetch(url)));
+        const data = await Promise.all(responses.map(res => res.json()));
+        return data.map((item, i) => ({
+            id: i,
+            name: item.hostname,
+             players: item.players.online
+        }));
+    } catch (error) {
+        console.log(`Erro ao obter informações da API: ${error}`);
+    }
+}
+
+setInterval(() => {
+    getServerData([
+        'https://api.mcsrvstat.us/2/aspectmania.com.br',
+        'https://api.mcsrvstat.us/2/hypixel.net',
+        'https://api.mcsrvstat.us/2/redestone.com',
+        'https://api.mcsrvstat.us/2/hylex.net',
+    ]).then(serverData => {
+        serverData.sort((a, b) => b.players - a.players);
+
+        let content = serverData.map((item, i) =>
+            `<p>${i + 1}. ${item.name} (${item.players} players) </p>`).join('');
+
+        var ranking = document.querySelector('.ranking');
+        ranking.innerHTML = content;
+    });
+}, 10000);
+
+let count = 10
+setInterval(() => {
+    count--;
+    document.querySelector('.time').innerHTML = count;
+    if(count === 0) {
+        count = 10;
+    }
+}, 1000);
