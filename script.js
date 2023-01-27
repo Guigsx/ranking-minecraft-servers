6d3dd0634333e7686738a44ff62c4bc1c2a9704f
Guigsz <guilhermeschmitzguii@gmail.com>
2023-01-27 13:23:08 -0300

    melhorias da api

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
+let api = 'https://api.mcstatus.io/v2/status/java/';
+const urls = [
+    `${api}aspectmania.com.br`,
+    `${api}redestone.com`,
+    `${api}hylex.net`,
+    `${api}mush.com.br`,
+    `${api}jogar.pixelmonbrasil.com.br`,
+    `${api}redefantasy.com`,
+]
 async function getServerData(urls) {
     try {
         const responses = await Promise.all(urls.map(url => fetch(url)));
         const data = await Promise.all(responses.map(res => res.json()));
         return data.map((item, i) => ({
             id: i,
-            name: item.hostname,
-            players: item.players.online
+            name: item.host,
+            players: item.players.online,
+            icon: item.icon,
         }));
     } catch (error) {
         console.log(`Erro ao obter informações da API: ${error}`);
     }
 }
-getServerData([
-    'https://api.mcsrvstat.us/2/aspectmania.com.br',
-    'https://api.mcsrvstat.us/2/hypixel.net',
-    'https://api.mcsrvstat.us/2/redestone.com',
-    'https://api.mcsrvstat.us/2/hylex.net',
-]).then(serverData => {
+getServerData(urls).then(serverData => {
     serverData.sort((a, b) => b.players - a.players);
 
     let content = serverData.map((item, i) =>
-        `<p>${i + 1}. ${item.name} (${item.players} players) </p>`).join('');
+        `<p><img src="${item.icon}"> ${i + 1}. ${item.name} (${item.players.toLocaleString()} players) </p>`).join('');
 
     document.querySelector('.ranking').innerHTML = content;
 });
 
 setInterval(() => {
-    getServerData([
-        'https://api.mcsrvstat.us/2/aspectmania.com.br',
-        'https://api.mcsrvstat.us/2/hypixel.net',
-        'https://api.mcsrvstat.us/2/redestone.com',
-        'https://api.mcsrvstat.us/2/hylex.net',
-    ]).then(serverData => {
+    getServerData(urls).then(serverData => {
         serverData.sort((a, b) => b.players - a.players);
 
         let content = serverData.map((item, i) =>
-            `<p>${i + 1}. ${item.name} (${item.players} players) </p>`).join('');
+            `<p><img src="${item.icon}"> ${i + 1}. ${item.name} (${item.players.toLocaleString()} players) </p>`).join('');
         document.querySelector('.ranking').innerHTML = content;
     });
 }, 10000);
