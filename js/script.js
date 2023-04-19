b4bc88b4fde8369531ad74f62d3a0f06ea227d6d
Guilherme <guilhermeschmitzguii@gmail.com>
2023-04-19 01:54:32 -0300

    Versão final.

--- js/script.js
+++ js/script.js
@@ -1,58 +1,6 @@
-const dominios = [
-    'aspectmania.com.br',
-    'redestone.com',
-    'hylex.net',
-    'mush.com.br',
-    'jogar.pixelmonbrasil.com.br',
-    'redefantasy.com',
-    'redevaison.tk',
-    'redeaqua.com',
-    'redevonix.com',
-    'redegarnix.com',
-    'jogar.heavenlymc.com.br',
-    'rede-feather.net',
-    'redescreen.com',
-    'rededark.com',
-    'jogar.rederevo.com',
-    'armamc.com',
-    'pokebrasil.net',
-    'redwins.com.br',
-    'jogar.mc-mastercraft.net',
-    'fallzpixelmon.com',
-    'stardix.com',
-    'jogar.rede-way.com',
-    'jogar.futurium.com.br',
-    'jogar.gg',
-    'pixelmonline.com',
-    'jogar.craftit.com.br',
-    'mc.sparklypower.net',
-    'gsgserver.com.br',
-    'jogar.absolutgg.com.br',
-    'jogar.austv.net',
-    'jogar.celestialpixelmon.com',
-    'jogar.redesgp.com',
-    'mc.instamc.com.br',
-    'elgae.net',
-    'chronos.craftlandia.com.br',
-    'potpvp.com.br',
-    'drazyh.com',
-    'redeforce.net',
-    'flamemc.com.br',
-    'bravemc.com.br',
-    'redesnow.com.br',
-    'arcanth.net',
-    'veanty.com',
-    'jogarhappy.com',
-    'redestory.com',
-];
-
 const api = 'https://api.mcstatus.io/v2/status/java/';
-
-const urls = dominios.map(dominio => `${api}${dominio}`);
-
 const ranking = document.querySelector('.ranking');
 const resposta = document.querySelector('.resposta');
-const voltar = document.querySelector('.voltar')
 
 async function getServerData(urls) {
     try {
@@ -68,7 +16,7 @@ async function getServerData(urls) {
                 players: players,
                 icon: icon,
                 online: online,
-            }
+            };
         });
     } catch (error) {
         console.log(`Erro ao obter informações da API: ${error}`);
@@ -76,21 +24,38 @@ async function getServerData(urls) {
 }
 
 function updateRanking(serverData) {
-    let total = 0;
-    serverData.sort((a, b) => (b.players) - (a.players));
-    let content = serverData.map((item, i) => {
-        total += item.players;
-        return `<p><n>${i + 1}</n> <icons><img width="64" height="64" alt="Imagem do servidor" src="${item.icon}"></icons>
-           <nome>${item.name}</nome>
-           <on>${item.players.toLocaleString()} online <onoff style="background-color: ${item.online};"></onoff></on></p>
-           `;
-    }).join('');
-    ranking.innerHTML = `<h1>Ranking</h1><h2>Total de jogadores online: <tot>${total.toLocaleString()}</tot></h2>` + content;
-    console.log(`Total de jogadores online: ${total.toLocaleString()}`);
+    fetch('./js/dominios.json')
+        .then(response => response.json())
+        .then(dominios => {
+            const urls = dominios.map(dominio => `${api}${dominio}`);
+
+            let total = 0;
+            serverData.sort((a, b) => (b.players) - (a.players));
+            let top10 = serverData.slice(0, 10);
+            let servidores = urls.length;
+            let content = top10.map((item, i) => {
+                total += item.players;
+                return `<p onclick="search('${item.name}')"><n>${i + 1}</n> <icons><img width="64" height="64" alt="Imagem do servidor" src="${item.icon}"></icons>
+      <nome>${item.name}</nome>
+      <on>${item.players.toLocaleString()} online <onoff style="background-color: ${item.online};"></onoff></on></p>
+      `;
+            }).join('');
+            ranking.innerHTML = `<h1>Ranking</h1><h2>Mais de <strong>${servidores}</strong> servidores monitorados.<br>Total de jogadores online: <tot style="color: rgb(0, 255, 13);"><strong>${total.toLocaleString()}</strong></tot></h2>` + content;
+            console.log(`Total de jogadores online: ${total.toLocaleString()}`);
+        });
 }
 
-getServerData(urls).then(updateRanking);
-
-setInterval(() => {
-    getServerData(urls).then(updateRanking);
-}, 60000);
+// Carrega os domínios a partir do arquivo JSON
+fetch('./js/dominios.json')
+    .then(response => response.json())
+    .then(dominios => {
+        const urls = dominios.map(dominio => `${api}${dominio}`);
+        console.log(urls.length);
+        getServerData(urls).then(updateRanking);
+        setInterval(() => {
+            getServerData(urls).then(updateRanking);
+        }, 60000);
+    })
+    .catch(error => {
+        console.log(`Erro ao carregar domínios: ${error}`);
+    });
--- js/search.js
+++ js/search.js
@@ -1,3 +1,31 @@
+const modal = document.querySelector('.modal')
+const fade = document.querySelector('.fade')
+
+function search(serverName) {
+    modal.classList.remove('esconder')
+    fade.classList.remove('esconder')
+    fetch(`https://api.mcstatus.io/v2/status/java/${serverName}`)
+        .then(res => res.json())
+        .then(data => {
+            let jogadores = `${data.players.online} / ${data.players.max}`
+            let icon = data.icon
+            let name = data.host
+            resposta.innerHTML = `
+        <img src="${icon}">
+        <name>${name}</name>
+        <players>${jogadores}</players>
+        `
+        }).catch(e => {
+            resposta.innerHTML = "Não encontrado"
+        })
+}
+
+document.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        document.getElementById("procurar").click();
+    }
+})
+
 function validateInputs() {
     var inputs = document.querySelectorAll('.input-1');
     var inputsCompleted = true;
@@ -9,10 +37,13 @@ function validateInputs() {
     return inputsCompleted;
 }
 
+fade.classList.add('esconder')
+modal.classList.add('esconder')
+
 function procurar() {
     if (validateInputs()) {
-        ranking.classList.add('esconder')
-        voltar.classList.remove('esconder')
+        modal.classList.remove('esconder')
+        fade.classList.remove('esconder')
         let input = document.querySelector('.input-1')
         fetch(`https://api.mcstatus.io/v2/status/java/${input.value}`)
             .then(res => res.json())
@@ -25,8 +56,10 @@ function procurar() {
                 <name>${name}</name>
                 <players>${jogadores}</players>
                 `
+                modal.querySelector('.loading').classList.add('esconder')
             }).catch(e => {
-                resposta.innerHTML = "Não encontrado"
+                resposta.innerHTML = `<img src="./imgs/compass.png"><p>Nada encontrado...</p>`
+                modal.querySelector('.loading').classList.add('esconder')
             })
 
     } else {
@@ -34,15 +67,10 @@ function procurar() {
     }
 }
 
-function back() {
-    voltar.classList.add('esconder')
-    ranking.classList.remove('esconder')
+
+function close_modal() {
+    modal.classList.add('esconder')
+    fade.classList.add('esconder')
 }
 
-document.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-        document.getElementById("procurar").click();
-    }
-})
-
-voltar.classList.add('esconder')
+fade.addEventListener('click', close_modal)
