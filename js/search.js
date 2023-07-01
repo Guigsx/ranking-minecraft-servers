887878ab39ae141905c9455706420ce13323d2ef
Guilherme <guilhermeschmitzguii@gmail.com>
2023-07-01 18:41:49 -0300

    commit

--- js/search.js
+++ js/search.js
@@ -45,28 +45,26 @@ function procurar() {
         modal.classList.remove('esconder')
         fade.classList.remove('esconder')
         let input = document.querySelector('.input-1')
-        fetch(`https://api.mcstatus.io/v2/status/java/${input.value}`)
-            .then(res => res.json())
-            .then(data => {
-                let jogadores = `${data.players.online} / ${data.players.max}`
-                let icon = data.icon
-                let name = data.host
-                resposta.innerHTML = `
+        try {
+            const response = fetch(`https://api.mcstatus.io/v2/status/java/${input.value}`)
+            const data = response.json();
+            let jogadores = `${data.players.online} / ${data.players.max}`
+            let icon = data.icon
+            let name = data.host
+            resposta.innerHTML = `
                 <img src="${icon}">
                 <name>${name}</name>
                 <players>${jogadores}</players>
                 `
-                modal.querySelector('.loading').classList.add('esconder')
-            }).catch(e => {
-                resposta.innerHTML = `<img src="./imgs/compass.png"><p>Nada encontrado...</p>`
-                modal.querySelector('.loading').classList.add('esconder')
-            })
+            modal.querySelector('.loading').classList.add('esconder')
+        } catch (error) {
+            resposta.innerHTML = `<img src="./imgs/compass.png"><p>Nada encontrado...</p>`
+        }
 
     } else {
         alert('Nada para procurar.')
     }
 }
-
 
 function close_modal() {
     modal.classList.add('esconder')
